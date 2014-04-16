893e8cdeaf5e39e664a617066d443d69cc3d1e1d
RightyHo <andrewho@192.168.1.68>
2014-04-16 21:10:17 +0100

    wrote code for QuizServerImpl & QuizStore classes, this time round I am approaching the project from the user perspective, imagining the functions that i will need and assigning them to the different classes as I go along.  a lot of the QuizServer methods will just be simple calls to the other supporting classes requesting the information from them and not doing much more than a middle man job

--- src/mach2/QuizServerImpl.java
+++ src/mach2/QuizServerImpl.java
@@ -1,6 +1,51 @@
 package mach2;
 
-public class QuizServerImpl {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuizServerImpl extends UnicastRemoteObject implements QuizServer, Serializable{
 	QuizStore quizStore;
 	InputOutput io;
+	
+	public QuizServerImpl() throws RemoteException {
+		io = new InputOutputImpl(fileName);
+		if(io.fileAlreadyExists()){
+			Object[] obj = io.readFromDisk();
+			quizStore = (QuizStore) obj[0];
+		} else {
+			quizStore = new QuizStoreImpl();
+		}
+	}
+	
+	Quiz createNewQuiz(String quizName){
+		return quizStore.createNewQuiz(quizName);
+	}
+	
+	Question createNewQuestion(inputQ,answerA,answerB,answerC,answerD,correctAnswer){
+		return 
+	}
+
+	List<String> getAvaliableQuizList(){
+		return quizStore.getAvailableQuizList();
+	}
+	
+	PlayerAttempt quizGameService.getQuiz(String quizName){
+		return quizStore.getQuiz(quizName);
+	}
+	
+	/**
+	 * Saves the quizStore to disk
+	 * @throws RemoteException
+	 */
+	public void flush() throws RemoteException{
+		io.saveToDisk(quizStore);
+	}
+	
+	@Override
+	public String echo(String s) throws RemoteException{
+		System.out.println("Replied to a client saying '" + s + "'");
+		return s;
+	}	
 }
--- src/mach2/QuizStoreImpl.java
+++ src/mach2/QuizStoreImpl.java
@@ -1,5 +1,9 @@
 package mach2;
 
-public class QuizStoreImpl implements QuizStore {
+public class QuizStoreImpl implements QuizStore,Serializable {
 	List<Quiz> quizList;
+	
+	public QuizStoreImpl(){
+		quizList = new ArrayList<Quiz>();
+	}
 }
